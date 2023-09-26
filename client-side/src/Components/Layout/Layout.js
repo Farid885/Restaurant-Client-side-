32183a11fc6fd1c7209e69a75016124bf7f0736c
Farid885 <ferid14zamanli@gmail.com>
2023-09-26 14:36:22 +0400

    respons

--- client-side/src/Components/Layout/Layout.js
+++ client-side/src/Components/Layout/Layout.js
@@ -1,7 +1,5 @@
 import React from "react";
-import About from "../Pages/about/About";
 import Header from "./Header";
-import Home from "../Pages/Home";
 import Footer from "../Layout/Footer";
 
 function Layout(props) {
