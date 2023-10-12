1eda7689c60525df25a77275cc46991cf37d72ea
Farid885 <ferid14zamanli@gmail.com>
2023-10-12 14:58:34 +0400

    add Datas to reservations

--- client-side/src/Components/Agent.js
+++ client-side/src/Components/Agent.js
@@ -35,6 +35,78 @@ const Categories = {
   Remove: (id) => requests.del(`/categories/${id}`),
 };
 
+const Persons = {
+  /**
+   * @returns {Promise<Array<{id:number;name:string}>>}
+   */
+  All: () => requests.get("/persons"),
+  /** @param {{name:string}} body */
+  Create: (body) => requests.post("/persons", body),
+  /**
+   * @param {number} id
+   * @param {{name:string}} body */
+  Update: (id, body) => requests.put(`/persons/${id}`, body),
+  /** @param {number} id */
+  Remove: (id) => requests.del(`/persons/${id}`),
+};
+
+const Table = {
+  /**
+   * @returns {Promise<Array<{id:number;name:string}>>}
+   */
+  All: () => requests.get("/tables"),
+  /** @param {{name:string}} body */
+  Create: (body) => requests.post("/tables", body),
+  /**
+   * @param {number} id
+   * @param {{name:string}} body */
+  Update: (id, body) => requests.put(`/tables/${id}`, body),
+  /** @param {number} id */
+  Remove: (id) => requests.del(`/tables/${id}`),
+};
+
+
+
+
+
+
+
+const Menu = {
+  /**
+   * @returns {Promise<Array<{id:number;name:string;image:string;category_id:number;price:number;category:string}>>}
+   */
+  All: () => requests.get("/menu"),
+  /** 
+   *@param {{name:string; image:string, category_id:number, price:number, category:string}} body
+    @returns {Promise<{id:number, name:string, image:string, category_id:number, price:number, category:string}>} */
+  Create: (body) => requests.post("/menu", body),
+  /**
+   * @param {number} id
+   * @param {{name:string;image:string, category_id:number, price:number, category:string}} body 
+  @returns {Promise<{id:number, name:string, image:string, category_id:number, price:number, category:string}>}
+   * 
+   * */
+
+  Update: (id, body) => requests.put(`/menu/${id}`, body),
+
+  /**
+   * @param {number} id
+   *  @returns {Promise<void>}
+   */
+
+  Remove: (id) => requests.del(`/menu/${id}`),
+  /**
+   * 
+   * @param {number} id 
+      @returns {Promise<{id:number, name:string, image:string, category_id:number, price:number, category:string}>}
+   */
+  Details: (id) => requests.get(`/menu/${id}`),
+
+  /** 
+    @returns {Promise<Array<{id:number, name:string, image:string, category_id:number, price:number, category:string}>>} */
+  GetByCategory: (categoryId) => requests.get(`/menu?category_id=${categoryId}`),
+};
+
 const Comments = {
   All: () => requests.get("/comments"),
   Create: (body) => requests.post("/comments", body),
@@ -42,4 +114,30 @@ const Comments = {
   Remove: (id) => requests.del(`/comments/${id}`),
 };
 
-export const agent = { Posts, Categories, Comments };
+
+const Users = {
+  /**
+   * @returns {Promise<Array<{id:number;name:string}>>}
+   */
+  All: () => requests.get("/users"),
+  /** @param {{name:string}} body */
+  Create: (body) => requests.post("/users", body),
+  /**
+   * @param {number} id
+   * @param {{name:string}} body */
+  Update: (id, body) => requests.put(`/users/${id}`, body),
+  /** @param {number} id */
+  Remove: (id) => requests.del(`/users/${id}`),
+};
+
+export const agent = { Posts, Categories, Comments, Persons, Table, Menu,Users };
+
+/**
+ *
+ * @param {number} a
+ * @param {number} b
+ * @returns {string}
+ */
+function concat(a, b) {
+  return (a + b).toString();
+}
--- client-side/src/Components/Pages/reservation/Form.js
+++ client-side/src/Components/Pages/reservation/Form.js
@@ -1,14 +1,60 @@
 import React from "react";
+import { agent } from "../../Agent";
+import { useState, useEffect } from "react";
+const initialFormState = {
+  name: "",
+  phone: "",
+  person: "",
+  date: "",
+  time: "",
+};
 
 function Form() {
+  const [inputState, setInputState] = useState(initialFormState);
+
+  useEffect(() => {
+    console.log(inputState);
+  }, [inputState]);
+
+  /**
+   *
+   * @param {React.ChangeEvent<HTMLInputElement>} event
+   */
+
+  function handleChangeInput(event) {
+    setInputState((prev) => ({
+      ...prev,
+      [event.target.name]: event.target.value,
+    }));
+  }
+
+  /**
+   *
+   * @param {React.FormEvent<HTMLFormElement>} event
+   */
+
+  const onFormSubmit = async (event) => {
+    console.log("event", event);
+    event.preventDefault();
+
+    console.log("inputState", inputState);
+    await agent.Users.Create(inputState);
+    setInputState(initialFormState)  //reset
+    
+
+
+  };
+
   return (
-    <form className="w-full lg:w-1/2 font-bigText anim in-up">
+    <form onSubmit={onFormSubmit} className="w-full lg:w-1/2 font-bigText anim in-up">
       <div className="border-b border-b-gray-300">
         <label className="text-sm uppercase tracking-wide" htmlFor="fName">
           full name
         </label>
         <input
-          name="fName"
+          value={inputState.name}
+          onChange={handleChangeInput}
+          name="name"
           id="fName"
           type="text"
           className="w-full outline-none"
@@ -20,7 +66,9 @@ function Form() {
           phone number
         </label>
         <input
-          name="tel"
+          value={inputState.phone}
+          onChange={handleChangeInput}
+          name="phone"
           id="tel"
           type="tel"
           className="w-full outline-none"
@@ -32,7 +80,9 @@ function Form() {
           persons
         </label>
         <input
-          name="num"
+          onChange={handleChangeInput}
+          name="person"
+          value={inputState.person}
           id="num"
           type="number"
           className="w-full outline-none"
@@ -44,13 +94,27 @@ function Form() {
           <label className="text-sm uppercase tracking-wide" htmlFor="date">
             date
           </label>
-          <input type="date" name="date" id="date" className="outline-none" />
+          <input
+            value={inputState.date}
+            onChange={handleChangeInput}
+            type="date"
+            name="date"
+            id="date"
+            className="outline-none"
+          />
         </div>
         <div className="flex gap-8 items-center">
           <label className="text-sm uppercase tracking-wide" htmlFor="time">
             time
           </label>
-          <input type="time" name="time" id="time" className="outline-none" />
+          <input
+            value={inputState.time}
+            onChange={handleChangeInput}
+            type="time"
+            name="time"
+            id="time"
+            className="outline-none"
+          />
         </div>
       </div>
       <div className="flex justify-end mt-8">
