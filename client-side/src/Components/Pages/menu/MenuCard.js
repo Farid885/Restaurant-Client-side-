381b1d52643852ead4d715a637d907dca294fe71
Farid885 <ferid14zamanli@gmail.com>
2023-11-10 12:50:05 +0400

    fixs

--- client-side/src/Components/Pages/menu/MenuCard.js
+++ client-side/src/Components/Pages/menu/MenuCard.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { agent } from "../../Agent";
 
-function MenuCard(props) {
+function MenuCard() {
   
 
   const [menuItem, setMenuItem] = useState([]);
